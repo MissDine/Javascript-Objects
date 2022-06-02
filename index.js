1a90e4493e4a126230587bfb97bc5a24eb372f9b
MissDine <enidnyatichi1212@gmail.com>
2022-06-02 10:20:56 +0300

    Feat:Edit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // OBJECTS
+let user = {
+    name: "Enid",
+    age : 25,
+    email:"enidnyatichi1212@gmail.com",
+    prpjects:["tv show app","budget tracker"],
+    gender:"Female"
+}
+console.log(user);
