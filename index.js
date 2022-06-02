df1559505b5fff7466550ca1ba9e78318bee3c25
MissDine <enidnyatichi1212@gmail.com>
2022-06-02 10:22:16 +0300

    Fixed Typo error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let user = {
     name: "Enid",
     age : 25,
     email:"enidnyatichi1212@gmail.com",
-    prpjects:["tv show app","budget tracker"],
+    projects:["tv show app","budget tracker"],
     gender:"Female"
 }
 console.log(user);
